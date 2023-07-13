301377d3213ef295278f6d9fb8085ce1529221f9
Joy A <itsjoy@gmail.com>
2023-07-13 17:39:31 +0530

    #73 | Add uuid and isVoided fields to BaseEntity

--- integration-data/src/main/java/org/avni_integration_service/integration_data/domain/framework/BaseEntity.java
+++ integration-data/src/main/java/org/avni_integration_service/integration_data/domain/framework/BaseEntity.java
@@ -1,6 +1,7 @@
 package org.avni_integration_service.integration_data.domain.framework;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 @MappedSuperclass
 public abstract class BaseEntity {
@@ -9,11 +10,34 @@ public abstract class BaseEntity {
     @Column(name = "id", updatable = false, nullable = false)
     private Integer id;
 
+    @Column
+    @NotNull
+    private String uuid;
+
+    @Column
+    private boolean isVoided;
+
     public Integer getId() {
         return id;
     }
 
     public void setId(Integer id) {
         this.id = id;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public boolean isVoided() {
+        return isVoided;
+    }
+
+    public void setVoided(boolean voided) {
+        isVoided = voided;
     }
 }
