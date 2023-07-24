ace4d9b988cd48160767c1049b9eec02a6e528c3
himeshr <himeshl@samanvayfoundation.org>
2023-07-24 14:14:04 +0530

    avniproject/goonj#63 | Correct UnclassifiedError error_types sql migration

--- integration-data/src/main/resources/db/migration/V2_3_8__Goonj_unclassified_dup_removal.sql
+++ integration-data/src/main/resources/db/migration/V2_3_8__Goonj_unclassified_dup_removal.sql
@@ -1,4 +1,4 @@
-DELETE FROM error_type where name = 'UnclassifiedError' and integration_system_id = 2;
+update error_record_log erl set error_type_id = (SELECT id FROM error_type where name = 'UnclassifiedError' and integration_system_id = 2 and comparison_operator is null)
+where error_type_id in (SELECT id FROM error_type where name = 'UnclassifiedError' and integration_system_id = 2 and comparison_operator is not null);
 
-INSERT INTO error_type (name, integration_system_id, comparison_operator, comparison_value)
-VALUES ('UnclassifiedError', 2, NULL, NULL);
+DELETE FROM error_type where name = 'UnclassifiedError' and integration_system_id = 2 and comparison_operator is not null;
