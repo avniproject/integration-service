2006fdd26cb9d04abef93b241033101b44fdc655
himeshr <himeshl@samanvayfoundation.org>
2023-11-30 17:54:06 +0530

    #94 | Do not create a new errorRecord every time the same student errors out, update it only id errorMsg changes

--- lahi/src/main/java/org/avni_integration_service/lahi/service/StudentErrorService.java
+++ lahi/src/main/java/org/avni_integration_service/lahi/service/StudentErrorService.java
@@ -34,25 +34,46 @@ public class StudentErrorService {
         saveStudentError(lahiStudent, throwable, LahiErrorType.CommonError);
     }
 
-    public void saveStudentError(Student lahiStudent, Throwable throwable, LahiErrorType lahiErrorType) {
+    public ErrorRecord saveStudentError(Student lahiStudent, Throwable throwable, LahiErrorType lahiErrorType) {
+        ErrorRecord errorRecord = getErrorRecord(lahiStudent);
         ErrorType errorType = getErrorType(lahiErrorType);
-        ErrorRecord errorRecord = new ErrorRecord();
-        errorRecord.setIntegratingEntityType("Student");
-        errorRecord.setIntegrationSystem(integrationSystemRepository.find());
-        errorRecord.setEntityId(lahiStudent.getFlowResultId());
-        errorRecord.setAvniEntityType(AvniEntityType.Subject);
-        errorRecord.setIntegratingEntityType(LahiEntityType.Student.name());
-        errorRecord.addErrorLog(errorType, throwable.getMessage());
-        errorRecord.setProcessingDisabled(false);
-        errorRecordRepository.saveErrorRecord(errorRecord);
+        String errorMsg = throwable.getMessage();
+        String lahiEntityType = LahiEntityType.Student.name();
+        String flowResultId = lahiStudent.getFlowResultId();
+        if (errorRecord != null && errorRecord.hasThisAsLastErrorTypeAndErrorMessage(errorType, errorMsg)) {
+            logger.info(String.format("Same error as the last processing for entity flowResultId %s, and type %s", flowResultId, lahiEntityType));
+            if (!errorRecord.isProcessingDisabled()) {
+                errorRecord.setProcessingDisabled(true);
+                errorRecordRepository.save(errorRecord);
+            }
+            errorRecord.addErrorLog(errorType, errorMsg);
+        } else if (errorRecord != null && !errorRecord.hasThisAsLastErrorTypeAndErrorMessage(errorType, errorMsg)) {
+            logger.info(String.format("New error for entity flowResultId %s, and type %s", flowResultId, lahiEntityType));
+            errorRecord.addErrorLog(errorType, errorMsg);
+            errorRecordRepository.save(errorRecord);
+        } else {
+            errorRecord = new ErrorRecord();
+            errorRecord.setIntegrationSystem(integrationSystemRepository.find());
+            errorRecord.setEntityId(lahiStudent.getFlowResultId());
+            errorRecord.setIntegratingEntityType(LahiEntityType.Student.name());
+            errorRecord.addErrorLog(errorType, errorMsg);
+            errorRecord.setProcessingDisabled(false);
+            errorRecordRepository.save(errorRecord);
+        }
+        return errorRecord;
     }
 
     public void deleteStudentError(Student lahiStudent) {
-        ErrorRecord errorRecord = errorRecordRepository.
-                findByIntegratingEntityTypeAndEntityId(LahiEntityType.Student.name(), lahiStudent.getFlowResultId());
+        ErrorRecord errorRecord = getErrorRecord(lahiStudent);
         if (errorRecord != null) {
             errorRecordRepository.delete(errorRecord);
         }
+    }
+
+    private ErrorRecord getErrorRecord(Student lahiStudent) {
+        ErrorRecord errorRecord = errorRecordRepository.
+                findByIntegratingEntityTypeAndEntityId(LahiEntityType.Student.name(), lahiStudent.getFlowResultId());
+        return errorRecord;
     }
 
     private ErrorType getErrorType(LahiErrorType errorType) {
