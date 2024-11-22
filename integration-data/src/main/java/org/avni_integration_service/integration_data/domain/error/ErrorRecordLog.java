e343c3541a3ee464dd620817112cd2e1d91c8ebe
himeshr <himeshl@samanvayfoundation.org>
2024-11-22 17:18:53 +0530

    avniproject/goonj#121 | Truncate errorMsg size to 1000 chars length to avoid bloating up the ErrorRecordLog table

--- integration-data/src/main/java/org/avni_integration_service/integration_data/domain/error/ErrorRecordLog.java
+++ integration-data/src/main/java/org/avni_integration_service/integration_data/domain/error/ErrorRecordLog.java
@@ -1,5 +1,6 @@
 package org.avni_integration_service.integration_data.domain.error;
 
+import io.micrometer.core.instrument.util.StringUtils;
 import org.avni_integration_service.integration_data.converter.MapToJsonConverter;
 import org.avni_integration_service.integration_data.domain.framework.BaseEntity;
 import org.hibernate.annotations.ColumnTransformer;
@@ -60,7 +61,7 @@ public class ErrorRecordLog extends BaseEntity {
     }
 
     public void setErrorMsg(String errorMsg) {
-        this.errorMsg = errorMsg;
+        this.errorMsg = StringUtils.isNotEmpty(errorMsg) ? errorMsg.substring(0, 1000) : errorMsg;
     }
 
     public Map<String, Object> getBody() {
