a713c3c25cf923a74590bd2d28350222576a39f5
himeshr <himeshl@samanvayfoundation.org>
2025-02-12 14:14:40 +0530

    avniproject/avni-server#825 | Update rwb nudge custom query

--- rwb/src/main/resources/custom_query_insert.sql
+++ rwb/src/main/resources/custom_query_insert.sql
@@ -1,9 +1,11 @@
-WITH primary_users as (
+INSERT INTO public.custom_query (id, uuid, name, query, organisation_id, is_voided, version, created_by_id,
+                                 last_modified_by_id, created_date_time, last_modified_date_time)
+VALUES (DEFAULT, '69f9f68d-7870-4ea4-b69f-49f68da0c17a', 'Inactive users', 'WITH primary_users as (
     select distinct u.id user_id, u.name first_name, catchment_id
     from users u
              join user_group ug on u.id = ug.user_id and ug.is_voided = false
              join groups g on g.id = ug.group_id and g.is_voided = false
-    where g.name = 'Primary Users'
+    where g.name = ''Primary Users''
       and u.is_voided = false
 ),
      work_orders as (
@@ -12,7 +14,7 @@ WITH primary_users as (
          where i.is_voided = false
            and i.subject_type_id = (select id
                                     from subject_type
-                                    where name = 'Work Order'
+                                    where name = ''Work Order''
                                       and organisation_id = (select id from organisation where db_user = :org_db_user)
                                       and not subject_type.is_voided)
      ),
@@ -22,7 +24,7 @@ WITH primary_users as (
                             where e.encounter_type_id =
                                   (select id
                                    from encounter_type
-                                   where name = 'Work order endline'
+                                   where name = ''Work order endline''
                                      and organisation_id = (select id from organisation where db_user = :org_db_user)
                                      and not encounter_type.is_voided)
                               and (e.is_voided is null or e.is_voided = false)
@@ -41,18 +43,18 @@ WITH primary_users as (
              OR count(wo.wo_id) > count(cwo.wo_id)
      ),
      active_user_ids as (select (case
-                                     when ind.created_date_time > TO_TIMESTAMP(:cutOffDate, 'YYYY-MM-DDTHH24:MI:ss.MS')
+                                     when ind.created_date_time > TO_TIMESTAMP(:cutOffDate, ''YYYY-MM-DDTHH24:MI:ss.MS'')
                                          then ind.created_by_id end) as cuid,
                                 ind.last_modified_by_id              as muid
                          from individual ind
-                         where ind.last_modified_date_time > TO_TIMESTAMP(:cutOffDate, 'YYYY-MM-DDTHH24:MI:ss.MS')
+                         where ind.last_modified_date_time > TO_TIMESTAMP(:cutOffDate, ''YYYY-MM-DDTHH24:MI:ss.MS'')
                          UNION
                          select (case
-                                     when enc.created_date_time > TO_TIMESTAMP(:cutOffDate, 'YYYY-MM-DDTHH24:MI:ss.MS')
+                                     when enc.created_date_time > TO_TIMESTAMP(:cutOffDate, ''YYYY-MM-DDTHH24:MI:ss.MS'')
                                          then enc.created_by_id end) as cuid,
                                 enc.last_modified_by_id              as muid
                          from encounter enc
-                         where enc.last_modified_date_time > TO_TIMESTAMP(:cutOffDate, 'YYYY-MM-DDTHH24:MI:ss.MS')
+                         where enc.last_modified_date_time > TO_TIMESTAMP(:cutOffDate, ''YYYY-MM-DDTHH24:MI:ss.MS'')
                          order by 1 asc)
 select distinct user_id, first_name
 from primary_users pu
@@ -61,4 +63,11 @@ where user_id not in (select cuid
                       from active_user_ids
                       union
                       select muid
-                      from active_user_ids);
+                      from active_user_ids);',
+        :org_id,
+        false,
+        0,
+        1,
+        1,
+        now(),
+        now());
